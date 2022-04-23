141ed31d8e9853628cccbb7ee7eb9315e3a5176a
Lebbar Abdelhadi <lebbar1996@gmail.com>
2022-04-23 18:56:35 +0000

    IOC

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -3,6 +3,8 @@ package com.mycompany.dvdstore;
 import com.mycompany.dvdstore.Service.DefaultMovieService;
 import com.mycompany.dvdstore.controller.MovieController;
 import com.mycompany.dvdstore.repository.FileMovieRepository;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
@@ -12,13 +14,17 @@ public class App
 {
     public static void main( String[] args )
     {
-        FileMovieRepository goLiveMovieRepository =new FileMovieRepository();
+      /*  FileMovieRepository goLiveMovieRepository =new FileMovieRepository();
         DefaultMovieService movieService =new DefaultMovieService();
         // we inject the class GoLiveMovieRepository instead of the interface in setMovieRepositoryInterface
         movieService.setMovieRepositoryInterface(goLiveMovieRepository);
         MovieController movieController =new MovieController();
         // we inject the class MovieService instead of the interface MovieServiceInterface
-        movieController.setMovieServiceInterface(movieService);
+        movieController.setMovieServiceInterface(movieService);*/
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        MovieController movieController = context.getBean(MovieController.class);
+
         movieController.addUsingConsole();
 
 
