7a4de35ac0c1f765424c1b3051ab31b29e2084fd
Lebbar Abdelhadi <lebbar1996@gmail.com>
2022-04-25 15:50:13 +0200

    Suuppresion d'AppConfig

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -3,12 +3,19 @@ package com.mycompany.dvdstore;
 import com.mycompany.dvdstore.controller.MovieController;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 /**
  * Hello world!
  *
  */
+@Configuration
+@ComponentScan(basePackages = {"com.mycompany.dvdstore.controller","com.mycompany.dvdstore.Service","com.mycompany.dvdstore.repository.file"})
+@PropertySource("classpath:application.properties")
 public class App 
 {
     public static void main( String[] args )
@@ -22,7 +29,7 @@ public class App
         movieController.setMovieServiceInterface(movieService);*/
 
         //ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
         MovieController movieController = context.getBean(MovieController.class);
 
         movieController.addUsingConsole();
